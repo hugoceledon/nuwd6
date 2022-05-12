a1b46578be49ff217fec9850af94ded2320a57f0
hugocele <hugo.celedon@nextonia.com.co>
2022-05-12 15:17:52 -0500

    feat: :fire: Added renderer

--- public/app.js
+++ public/app.js
@@ -1,24 +1,184 @@
-//Inicializador del elemento Slider
-$("#rangoPrecio").ionRangeSlider({
-  type: "double",
-  grid: false,
-  min: 0,
-  max: 100000,
-  from: 1000,
-  to: 20000,
-  prefix: "$"
-})
+(
+  function(doc, win, $){
+      (function (){
+          return Seeker = {
+              apiUrl: '/seeker',
+              customSearch: false,
+              $propList: $('.lista'),
+              $tipo: $('#tipo'),
+              $ciudad: $('#ciudad'),
+              $busqueda: $('#checkPersonalizada'),
+              $personalizada: $('#personalizada'),
+              $buscador: $('#buscar'),
+              $rangoPrecio: $("#rangoPrecio"),
+              $propList: $('.lista'),
+              propTemplate: '<div class="card horizontal">' +
+                   '<div class="card-image">' +
+                   '<img src="img/home.jpg">' +
+                   '</div>' +
+                   '<div class="card-stacked">' +
+                   '<div class="card-content">' +
+                   '<div>' +
+                   '<b>Direccion: </b><p>:ADDR:</p>' +
+                   '</div>' +
+                   '<div>' +
+                   '<b>Ciudad: </b><p>:CITY:</p>' +
+                   '</div>' +
+                   '<div>' +
+                   '<b>Telefono: </b><p>:PHONE:</p>' +
+                   '</div>' +
+                   '<div>' +
+                   '<b>Código postal: </b><p>:ZIPCODE:</p>' +
+                   '</div>' +
+                   '<div>' +
+                   '<b>Precio: </b><p>:PRICE:</p>' +
+                   '</div>' +
+                   '<div>' +
+                   '<b>Tipo: </b><p>:TYPE:</p>' +
+                   '</div>' +
+                   '</div>' +
+                   '<div class="card-action right-align">' +
+                   '<a href="#">Ver más</a>' +
+                   '</div>' +
+                   '</div>' +
+                   '</div>',
 
-function setSearch() {
-  let busqueda = $('#checkPersonalizada')
-  busqueda.on('change', (e) => {
-    if (this.customSearch == false) {
-      this.customSearch = true
-    } else {
-      this.customSearch = false
-    }
-    $('#personalizada').toggleClass('invisible')
-  })
-}
+              Init: function(){
+                  var self = this
+                  self.cityOptions = []
+                  self.typeOptions = []
+                  self.getOptions()
+                  self.setSearch()
+                  self.startSeekBtn()
+              },
+              startSeekBtn: function(){
+                var self = this
+                self.$buscador.on('click', (e) =>{
+                  if(self.customSearch){
+                    let slider = self.$rangoPrecio.data("ionRangeSlider");
+                    let sliderFrom = slider.result.from
+                    let sliderTo = slider.result.to
+                    let selType = self.$tipo.val()
+                    if(selType == ""){
+                      selType = null
+                    }
+                    let selCity = self.$ciudad.val()
+                    if(selCity == ""){
+                      selCity = null
+                    }
+                    let payload = JSON.stringify({ 
+                      Ciudad: selCity, 
+                      Tipo: selType,
+                      PrecioMin: sliderFrom,
+                      PrecioMax: sliderTo 
+                    })
+                    self.ajaxRequest(self.apiUrl, 'POST', payload)
+                          .done(function(data){
+                            self.renderProps(data)
+                          }).fail(function(err){
+                              alert(err)
+                          })
+                  }else{
+                    self.ajaxRequest(self.apiUrl, 'GET', {})
+                          .done(function(data){
+                            self.renderProps(data)
+                          }).fail(function(err){
+                              alert(err)
+                          })
+                  }
+                })
+              },
+              renderProps: function(data){
+                var self = this
+                self.$propList.empty()
+                self._renderProp()
+              },
+              _renderProp: function(){
+                var self = this
+                var property = self.propTemplate.replace(':ADDR:', 'Calle 1')
+                                  .replace(':CITY:', 'Calle 1')
+                                  .replace(':PHONE:', '12348712841')
+                                  .replace(':ZIPCODE:', '1234567')
+                                  .replace(':PRICE:', '$1200')
+                                  .replace(':TYPE:', 'Casa')
+                self.$propList.append(property)
+              },
+              setSearch: function(){
+                var self = this
+                self.$busqueda.on('change', (e) => {
+                  if (this.customSearch == false) {
+                    self.customSearch = true
+                  } else {
+                    self.customSearch = false
+                  }
+                  self.$personalizada.toggleClass('invisible')
+                })
+              },
+              getOptions: function(){
+                  var self = this
+                  var endpoint = self.apiUrl + '/types'
+                  self.ajaxRequest(endpoint, 'GET', {})
+                          .done(function(data){
+                              self.appendTypes(data)
+                          }).fail(function(err){
+                              alert(err)
+                          })
+                  var endpoint = self.apiUrl + '/cities'
+                  self.ajaxRequest(endpoint, 'GET', {})
+                          .done(function(data){
+                              self.appendCities(data)
+                          }).fail(function(err){
+                              alert(err)
+                          })
+                  var endpoint = self.apiUrl + '/prices'
+                  self.ajaxRequest(endpoint, 'GET', {})
+                          .done(function(data){
+                              self.appendPrices(data)
+                          }).fail(function(err){
+                              alert(err)
+                          })
+              },
+              ajaxRequest: function(url, type, data){
+                  return $.ajax({
+                      url: url,
+                      type: type,
+                      data: data,
+                      contentType: "application/json; charset=utf-8",
+                      dataType: "json"
+                  })
+              },
+              _appendCity: function(city){
+                  $('#ciudad').append(`<option value="${city}">${city}</option>`);
+              },
+              _appendType: function(ty){
+                  $('#tipo').append(`<option value="${ty}">${ty}</option>`)
+              },
+              appendCities: function(data){
+                  var self = this
+                  for (let i = 0; i < data.length; i++) {
+                    self._appendCity(data[i])
+                  }
+              },
+              appendTypes: function(data){
+                  var self = this
+                  for (let i = 0; i < data.length; i++) {
+                    self._appendType(data[i])
+                  }
+              },
+              appendPrices: function(data){                
+                var self = this
+                self.$rangoPrecio.ionRangeSlider({
+                  type: "double",
+                  grid: false,
+                  min: data['min'],
+                  max: data['max'],
+                  from: data['min'],
+                  to: data['max'],
+                  prefix: "$"
+                })
+              }
+          }
+      })()
+  Seeker.Init()
+})(document, window, jQuery);
 
-setSearch()
